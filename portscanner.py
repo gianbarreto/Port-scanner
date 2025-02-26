e7d01f4cdd2dda9714a6249520c58b09a0657f9c
unknown <gmb.gian@gmail.com>
2025-02-26 20:52:09 -0300

    Primer commit.

---
+++ portscanner.py
@@ -0,0 +1,18 @@
+import socket
+
+ip = input("Enter the IP address: ")
+
+for port in range(1, 65535):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)
+
+    result = s.connect_ex((ip, port))
+
+    if result == 0:
+        print(f"Port {port} is open.")
+        s.close()
+    else:
+        print(f"Port {port} is closed.")
+
+# This program will try to connect to every port on the given IP address. If the connection is successful, it will print that the port is open.
+# The connect_ex() method is used to connect to the port. If the connection is successful, it returns 0, otherwise it returns an error code.
